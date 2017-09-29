43a64b8fb4819bd6287c2ccf7041fd1dd6f3fc2e
Levin Faber <LFA@timetoact.group>
2017-09-29 11:25:33 +0200

    Draft

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 var express = require('express');
 var app = express();
-var server_port = process.env.OPENSHIFT_NODEJS_PORT 
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP
+var request = require('request');
+var server_port = 80;
+var server_ip_address = "0.0.0.0"; 
+var url = "http://api.steampowered.com/ISteamUser/GetPlayerBans/v1/?key=AAD42D5DE55845AFF3E16ED82A426E36&steamids=76561198077704323";
 
 app.use(express.static("public"));
+
 app.get("/", function(req, res){
     res.sendFile(__dirname +"/"+ "index.html");
 });
+
+request.get(url, function(error, response, body){
+    console.log(body);
+});
+
 var server = app.listen(server_port, server_ip_address, function () {
    var host = server.address().address
    var port = server.address().port
    
    console.log("Example app listening at http://%s:%s", server_ip_address, server_port)
-})
+});
