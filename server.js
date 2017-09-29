7fe5cb8723b4b3558b7e9f1031744b048696aa02
Levin Faber <LFA@timetoact.group>
2017-09-29 12:07:28 +0200

    HTML

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 var express = require('express');
 var app = express();
 var request = require('request');
-var server_port = 80;
-var server_ip_address = "0.0.0.0"; 
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+
 var url = "http://api.steampowered.com/ISteamUser/GetPlayerBans/v1/?key=AAD42D5DE55845AFF3E16ED82A426E36&steamids=76561198077704323";
 
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
-    res.sendFile(__dirname +"/"+ "index.html");
+    res.sendFile(__dirname +"/"+ "/public/index.html");
 });
 
 request.get(url, function(error, response, body){
